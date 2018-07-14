77f136abf3e73f65bfed0a793c89b5cf9fc235a7
nskom <omrecordzz@gmail.com>
2018-07-14 22:04:27 +0700

    новые функции

--- sandbox/src/main/java/ru/nskom/sandbox/FirstJava.java
+++ sandbox/src/main/java/ru/nskom/sandbox/FirstJava.java
@@ -2,6 +2,15 @@ package ru.nskom.sandbox;
 
 public class FirstJava {
 
-	public static void main(String[] args) { System.out.println("Hello, world!");
-}
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4, 6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+    }
 }
---
+++ sandbox/src/main/java/ru/nskom/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.nskom.sandbox;
+
+public class Square {
+    public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
